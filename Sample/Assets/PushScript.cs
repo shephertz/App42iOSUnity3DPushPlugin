4cc9a82d07dfb1e0a03b6a320ec4db2a411a7fe1
Rajeev Ranjan <rajeev.ranjan@shephertz.co.in>
2014-03-27 16:20:40 +0530

    Sample Updated

--- Sample/Assets/PushScript.cs
+++ Sample/Assets/PushScript.cs
@@ -7,8 +7,8 @@ using System.Runtime.InteropServices;
 
 public class PushScript : MonoBehaviour 
 {
-	const string api_key = "67359321652c10b15fd5f659d096a2051745aa4f339b936d6ce5dccb165de863";
-	const string secret_key = "2d44878dd428046f23bd4b3807ce83e982db9900f4c5a8305255da4764c76610";
+	const string api_key = "Your_API_Key";
+	const string secret_key = "Your_Secret_Key";
 	PushResponse callBack = new PushResponse();
 	[System.Runtime.InteropServices.DllImport("__Internal")]
 	extern static public void registerForRemoteNotifications();
